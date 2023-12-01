9320bffa6f22fb324c18fa214a126ad67835d700
poevoid <65972401+poevoid@users.noreply.github.com>
2023-11-30 19:09:03 -0600

    Update DemonCore.ino

--- DemonCore/DemonCore.ino
+++ DemonCore/DemonCore.ino
@@ -1,15 +1,18 @@
 #include <Arduboy2.h>
 Arduboy2 arduboy;
+BeepPin1 geiger;
 #include "func.h"
 
 void setup() {
   // put your setup code here, to run once:
   arduboy.begin();
+  geiger.begin();
   arduboy.setFrameRate(60);
 }
 
 void loop() {
   if (!arduboy.nextFrame()){return;}
+  geiger.timer();
   arduboy.clear();
   arduboy.pollButtons();
   gameloop();
